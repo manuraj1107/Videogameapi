2cf19e428d36662b24129ca2e005ded1557640bd
manuraj1107 <manurajrockstar2000@gmail.com>
2022-07-27 00:31:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+
+import './App.css';
+import {useState, useEffect} from 'react';
+import useSWR from 'swr';
+
+
+const fetcher = (...args) => fetch(...args).then((response) => response.json())
+
+function App() {
+
+  const [gameTitle, setGameTitle] = useState('');
+  const [searchedGames, setSeachedGames] = useState([]);
+  const [gameDeals, setGameDeals] = useState([]);
+
+  const {data, error} = useSWR('https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=20&pageSize=3',fetcher);
+
+  const searchGame = () => {
+     fetch(`https://www.cheapshark.com/api/1.0/games?title=${gameTitle}&limit=3`)
+     .then((response) => response.json())
+     .then((data) => {
+      setSeachedGames(data);
+     })
+  };
+
+
+  
+  return (
+    <div className="App">
+      <div className="searchSection">
+        <h1>Search For A Game</h1>
+        <input type="text" placeholder="Minecraft..." 
+        onChange={(event)=> {
+          setGameTitle(event.target.value);
+          }} />
+          <button onClick={searchGame}>Search Game Title</button>
+
+          <div className='games'>
+            {searchedGames.map((game, key) => {
+              return (<div className='game' key={key}> 
+              {game.external} 
+              <img src={game.thumb}/>
+              {game.cheapest}
+              </div>
+              );
+            })}
+          </div>
+      </div>
+      <div className="dealsSection">
+        <h1> Latest Deals </h1>
+        <div className='games'>
+        {data && data.map((game, key) => {
+          return(
+            <div className='game' id='deals' key={key}>
+            
+            <h3>{game.title}</h3>
+            <p>Normal Price: {game.normalPrice}</p>
+            <p>Deal Price: {game.salePrice}</p>
+            <h3>YOU SAVED {game.savings.substr(0,2)}% </h3>
+            </div>
+          )
+        })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
